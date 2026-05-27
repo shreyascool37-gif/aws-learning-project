6b2b9a2ab9b9abf686493753b1701ef90f063aec
shreyascool37-gif <shreyascool37@gmail.com>
2026-05-27 10:37:07 +0530

    Add Fargate workflow

---
+++ app/app.js
@@ -0,0 +1,15 @@
+﻿const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Fargate running Node.js!');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(PORT, () => {
+  console.log(Server running on port );
+});
